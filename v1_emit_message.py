13ec63627c4bebc407b06dff0df1d6fdc548d97c
presleyschumacher <presley.schumacher2@gmail.com>
2023-02-02 12:32:55 -0600

    two versions

--- v1_emit_message.py
+++ v1_emit_message.py
@@ -1,23 +1,28 @@
 """
     This program sends a message to a queue on the RabbitMQ server.
 
-    Author: Denise Case
-    Date: January 14, 2023
+    Presley Schumacher
+    January 31, 2023
 
 """
 
 # add imports at the beginning of the file
 import pika
 
-# create a blocking connection to the RabbitMQ server
-conn = pika.BlockingConnection(pika.ConnectionParameters("LOCALHOST"))
-# use the connection to create a communication channel
+#The first program, send.py, will send a single message to the queue.
+
+# Establish a connection with RabbitMQ Server
+conn = pika.BlockingConnection(pika.ConnectionParameters('LocalHost'))
 ch = conn.channel()
-# use the channel to declare a queue
+# Now we're connnected to a broker on the local machine.
+# Before sending a message, we need to make sure the recipient queue exists
+# We will create a hello queue to which the message(s) will be delivered
 ch.queue_declare(queue="hello")
-# use the channel to publish a message to the queue
+# We are ready to send our first message
+# In RabbitMQ, the message goes through an exchange before being sent to the queue.
+# We will use the default exchange identified by an empty string. The queue name is specified in the routing_key parameter 
 ch.basic_publish(exchange="", routing_key="hello", body="Hello World!")
-# print a message to the console for the user
+# Print the message
 print(" [x] Sent 'Hello World!'")
-# close the connection to the server
+# Close the connection
 conn.close()
